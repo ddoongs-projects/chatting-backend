8620b2a90010d185a8754bc9efb6466a7408c064
김덕윤 <duk9741@gmail.com>
2025-08-05 02:30:54 +0900

    init project

---
+++ src/test/groovy/com/ddoongs/chatting/ChattingApplicationSpec.groovy
@@ -0,0 +1,15 @@
+package com.ddoongs.chatting
+
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class ChattingApplicationSpec extends Specification {
+
+    def "context loads"() {
+        expect:
+        true
+    }
+
+}
