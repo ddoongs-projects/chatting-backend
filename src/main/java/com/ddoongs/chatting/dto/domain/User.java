13b280822809ac395ce68615756e6be7f0c94de8
김덕윤 <duk9741@gmail.com>
2025-08-09 03:04:41 +0900

    feat: User 도메인 DTO 및 Projection 추가
    - User 도메인 객체 추가
    - UsernameProjection 인터페이스 추가 (JPA Projection용)

---
+++ src/main/java/com/ddoongs/chatting/dto/domain/User.java
@@ -0,0 +1,23 @@
+package com.ddoongs.chatting.dto.domain;
+
+import java.util.Objects;
+
+public record User(UserId userId, String username) {
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    User user = (User) o;
+    return Objects.equals(userId, user.userId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(userId);
+  }
+}
---
+++ src/main/java/com/ddoongs/chatting/dto/projection/UsernameProjection.java
@@ -0,0 +1,7 @@
+package com.ddoongs.chatting.dto.projection;
+
+public interface UsernameProjection {
+
+  String getUsername();
+
+}
