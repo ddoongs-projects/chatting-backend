0ae850aea9863db51076558d55fa618eca3614bf
김덕윤 <duk9741@gmail.com>
2025-08-09 01:50:16 +0900

    chore: 프로젝트 설정 업데이트
    - build.gradle: Spring Security, Redis, JWT 의존성 추가
    - application.yml: Redis, JPA, Security 설정 추가
    - schema.sql: 사용자 테이블 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,3 +9,16 @@ CREATE TABLE IF NOT EXISTS chat
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS chat_user
+(
+    user_id    BIGINT AUTO_INCREMENT,
+    username   VARCHAR(20)  NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP    NOT NULL,
+    updated_at TIMESTAMP    NOT NULL,
+    PRIMARY KEY (user_id),
+    CONSTRAINT unique_username UNIQUE (username)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
