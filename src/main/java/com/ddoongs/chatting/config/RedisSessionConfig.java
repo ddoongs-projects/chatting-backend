d06e5e8f61aff9b87e5c19398de67961aae30807
김덕윤 <duk9741@gmail.com>
2025-08-09 01:47:34 +0900

    feat: Redis 기반 세션 관리 설정 추가
    분산 환경에서의 세션 공유를 위한 Redis 설정

---
+++ src/main/java/com/ddoongs/chatting/config/RedisSessionConfig.java
@@ -0,0 +1,27 @@
+package com.ddoongs.chatting.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.security.jackson2.SecurityJackson2Modules;
+import org.springframework.session.FlushMode;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+// @EnableRedisIndexedHttpSession // 추가기능
+@EnableRedisHttpSession(
+    redisNamespace = "chatting:user_session",
+    maxInactiveIntervalInSeconds = 300,
+    flushMode = FlushMode.IMMEDIATE
+)
+@Configuration
+public class RedisSessionConfig {
+
+  @Bean
+  public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.registerModules(SecurityJackson2Modules.getModules(getClass().getClassLoader()));
+    return new GenericJackson2JsonRedisSerializer(mapper);
+  }
+}
