6fde0880473a696cf19107b5e938f321b4058f1d
김덕윤 <duk9741@gmail.com>
2025-08-11 00:55:08 +0900

    feat: UserChannelRepository에 채널 참여자 조회 메서드 추가
    - findUserIdByChannelId: 특정 채널의 모든 참여자 ID 조회
    - UserIdProjection을 사용한 효율적인 조회

--- src/main/java/com/ddoongs/chatting/repository/UserChannelRepository.java
+++ src/main/java/com/ddoongs/chatting/repository/UserChannelRepository.java
@@ -1,11 +1,16 @@
 package com.ddoongs.chatting.repository;
 
+import com.ddoongs.chatting.dto.projection.UserIdProjection;
 import com.ddoongs.chatting.entity.UserChannelEntity;
 import com.ddoongs.chatting.entity.UserChannelId;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
 
 public interface UserChannelRepository extends JpaRepository<UserChannelEntity, UserChannelId> {
 
   boolean existsByUserIdAndChannelId(@NonNull Long userId, @NonNull Long channelId);
+
+  List<UserIdProjection> findUserIdByChannelId(@NonNull Long channelId);
+
 }
