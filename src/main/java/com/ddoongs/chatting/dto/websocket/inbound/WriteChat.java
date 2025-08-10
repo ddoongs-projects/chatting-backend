e816a9d07ef66f5c325976422079c13804c1efac
김덕윤 <duk9741@gmail.com>
2025-08-11 02:49:45 +0900

    refactor: WriteChat DTO에서 username 필드 제거
    - 사용자 정보는 WebSocket 세션에서 관리
    - channelId와 content만 받도록 간소화

--- src/main/java/com/ddoongs/chatting/dto/websocket/inbound/WriteChat.java
+++ src/main/java/com/ddoongs/chatting/dto/websocket/inbound/WriteChat.java
@@ -8,26 +8,19 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class WriteChat extends BaseRequest {
 
   private final ChannelId channelId;
-  private final String username;
   private final String content;
 
   @JsonCreator
   public WriteChat(
       @JsonProperty("channelId") ChannelId channelId,
-      @JsonProperty("username") String username,
       @JsonProperty("content") String content) {
     super(MessageType.WRITE_CHAT);
     this.channelId = channelId;
-    this.username = username;
     this.content = content;
   }
 
   public ChannelId getChannelId() {
     return channelId;
-  }
-
-  public String getUsername() {
-    return username;
   }
 
   public String getContent() {
