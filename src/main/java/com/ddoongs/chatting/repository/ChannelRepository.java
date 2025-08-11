cd669db757bf04c626f35c204378218ea0c8a9e6
김덕윤 <duk9741@gmail.com>
2025-08-11 23:30:15 +0900

    feat: Repository 계층에 채널 관련 쿼리 메서드 추가
    - ChannelRepository에 채널 조회 및 초대 코드 관련 메서드 추가
    - UserChannelRepository 쿼리 메서드 개선

--- src/main/java/com/ddoongs/chatting/repository/ChannelRepository.java
+++ src/main/java/com/ddoongs/chatting/repository/ChannelRepository.java
@@ -1,10 +1,13 @@
 package com.ddoongs.chatting.repository;
 
+import com.ddoongs.chatting.dto.projection.ChannelProjection;
 import com.ddoongs.chatting.dto.projection.ChannelTitleProjection;
 import com.ddoongs.chatting.dto.projection.InviteCodeProjection;
 import com.ddoongs.chatting.entity.ChannelEntity;
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.lang.NonNull;
 
 public interface ChannelRepository extends JpaRepository<ChannelEntity, Long> {
@@ -12,4 +15,10 @@ public interface ChannelRepository extends JpaRepository<ChannelEntity, Long> {
   Optional<ChannelTitleProjection> findTitleByChannelId(@NonNull Long channelId);
 
   Optional<InviteCodeProjection> findInviteCodeByChannelId(@NonNull Long channelId);
+
+  Optional<ChannelProjection> findChannelCodeByInviteCode(@NonNull String inviteCode);
+
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  Optional<ChannelEntity> findForUpdateByChannelId(@NonNull Long channelId);
+
 }
--- src/main/java/com/ddoongs/chatting/repository/UserChannelRepository.java
+++ src/main/java/com/ddoongs/chatting/repository/UserChannelRepository.java
@@ -1,10 +1,13 @@
 package com.ddoongs.chatting.repository;
 
+import com.ddoongs.chatting.dto.projection.ChannelProjection;
 import com.ddoongs.chatting.dto.projection.UserIdProjection;
 import com.ddoongs.chatting.entity.UserChannelEntity;
 import com.ddoongs.chatting.entity.UserChannelId;
+import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
 
 public interface UserChannelRepository extends JpaRepository<UserChannelEntity, UserChannelId> {
@@ -13,4 +16,11 @@ public interface UserChannelRepository extends JpaRepository<UserChannelEntity, 
 
   List<UserIdProjection> findUserIdByChannelId(@NonNull Long channelId);
 
+  @Query("SELECT c.channelId AS channelId, c.title as title, c.headCount as hadCount "
+         + "FROM UserChannelEntity uc "
+         + "INNER JOIN ChannelEntity c ON uc.channelId = c.channelId "
+         + "WHERE uc.userId = :userId")
+  List<ChannelProjection> findChannelsByUserId(@NonNull @Param("userId") Long userid);
+
+  void deleteByUserIdAndChannelId(@NonNull Long userId, @NonNull Long channelId);
 }
